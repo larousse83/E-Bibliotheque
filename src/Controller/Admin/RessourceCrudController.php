e4703eda6d38891e895701b61108a95addedd941
larousse83 <laure.cambours@gmail.com>
2021-01-09 22:04:20 +0100

    add vignette properties and image file and change upload vignette on dashboard

--- src/Controller/Admin/RessourceCrudController.php
+++ src/Controller/Admin/RessourceCrudController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Ressource;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class RessourceCrudController extends AbstractCrudController
 {
@@ -16,11 +20,29 @@ class RessourceCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        return [
-            TextField::new('titre'),
-            AssociationField::new( 'ouvrage', 'Ouvrage' )->setCustomOption( "by_reference", false ),
-            AssociationField::new( 'chapitre', 'Chapitre' )->setCustomOption( "by_reference", false ),
-            AssociationField::new( 'section', 'Section' )->setCustomOption( "by_reference", false ),
-        ];
+        switch ($pageName){
+            case Crud::PAGE_NEW:
+            case Crud::PAGE_EDIT:
+                return [
+                    TextField::new('titre'),
+                    TextareaField::new('imageFile','vignette')->setFormType(VichImageType::class),
+                    TextField::new('fichier'),
+
+                    AssociationField::new( 'ouvrage', 'Ouvrage' )->setCustomOption( "by_reference", false ),
+                    AssociationField::new( 'chapitre', 'Chapitre' )->setCustomOption( "by_reference", false ),
+                    AssociationField::new( 'section', 'Section' )->setCustomOption( "by_reference", false ),
+                ];
+            default:
+                return [
+                    TextField::new('titre'),
+                    ImageField::new('vignette','vignette')->setBasePath('/medias/vignettes'),
+                    TextField::new('fichier'),
+
+                    AssociationField::new( 'ouvrage', 'Ouvrage' )->setCustomOption( "by_reference", false ),
+                    AssociationField::new( 'chapitre', 'Chapitre' )->setCustomOption( "by_reference", false ),
+                    AssociationField::new( 'section', 'Section' )->setCustomOption( "by_reference", false ),
+                ];
+        }
+
     }
 }
--- src/Entity/Ressource.php
+++ src/Entity/Ressource.php
@@ -2,14 +2,22 @@
 
 namespace App\Entity;
 
+use App\Entity\Traits\Timestampable;
 use App\Repository\RessourceRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=RessourceRepository::class)
+ * @Vich\Uploadable
+ * @ORM\HasLifecycleCallbacks
  */
 class Ressource extends ElementFavorisable
 {
+    use Timestampable;
+
     /**
      * @ORM\ManyToOne(targetEntity=Section::class, inversedBy="ressources")
      * @ORM\JoinColumn(nullable=false)
@@ -37,6 +45,15 @@ class Ressource extends ElementFavorisable
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $fichier;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="vignette_file", fileNameProperty="vignette")
+     * @Assert\Image(maxSize="8M")
+     * @var File|null
+     */
+    private $imageFile;
 
     public function getSection(): ?Section
     {
@@ -84,6 +101,30 @@ class Ressource extends ElementFavorisable
         $this->vignette = $vignette;
 
         return $this;
+    }
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdatedAt(new \DateTimeImmutable);
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
     }
 
     public function getFichier(): ?string
