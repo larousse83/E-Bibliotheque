450823c1d6995d437e96e0335e1fc339b0e666db
larousse83 <laure.cambours@gmail.com>
2021-01-10 21:04:22 +0100

    add css for flash message and add control on show page ouvrage

--- src/Controller/OuvrageController.php
+++ src/Controller/OuvrageController.php
@@ -15,6 +15,12 @@ class OuvrageController extends AbstractController
      */
     public function show(Ouvrage $ouvrage): Response
     {
+        if(!$this->getUser()){
+            $this->addFlash('error', 'Vous devez vous connecter !.');
+
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('ouvrage/show.html.twig', compact('ouvrage'));
     }
 }
