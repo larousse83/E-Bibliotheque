9118eec6404f464e612cab34675e12c76212c8f4
larousse83 <laure.cambours@gmail.com>
2021-01-10 21:29:14 +0100

    change show route and custome ouvrage pages

--- src/Controller/OuvrageController.php
+++ src/Controller/OuvrageController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class OuvrageController extends AbstractController
 {
     /**
-     * @Route("/ouvrage/{id}", name="app_ouvrages_show", methods={"GET"}))
+     * @Route("/ouvrage/show/{id}", name="app_ouvrages_show", methods={"GET"}))
      */
     public function show(Ouvrage $ouvrage): Response
     {
@@ -22,5 +22,19 @@ class OuvrageController extends AbstractController
         }
 
         return $this->render('ouvrage/show.html.twig', compact('ouvrage'));
+    }
+
+    /**
+     * @Route("/ouvrage/{id}", name="app_ouvrage", methods={"GET"}))
+     */
+    public function index(Ouvrage $ouvrage): Response
+    {
+        if(!$this->getUser()){
+            $this->addFlash('error', 'Vous devez vous connecter !.');
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('ouvrage/index.html.twig', compact('ouvrage'));
     }
 }
