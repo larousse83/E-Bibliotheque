2ab91bd2931a55d80c95f93f3e4491c84675b170
larousse83 <laure.cambours@gmail.com>
2021-01-12 10:36:01 +0100

    add jquery on app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,19 +9,7 @@
 import '../scss/app.scss';
 
 // start the Stimulus application
+import $ from 'jquery';
 import 'bootstrap';
 
-// Favorite Button - Heart
-$('.favme').click(function() {
-    $(this).toggleClass('active');
-});
 
-/* when a user clicks, toggle the 'is-animating' class */
-$(".favme").on('click touchstart', function(){
-    $(this).toggleClass('is_animating');
-});
-
-/*when the animation is over, remove the class*/
-$(".favme").on('animationend', function(){
-    $(this).toggleClass('is_animating');
-});
