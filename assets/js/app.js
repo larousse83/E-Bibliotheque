81c6fd74031438e9fcb67371e56c4da319091b70
larousse83 <laure.cambours@gmail.com>
2021-01-12 08:52:23 +0100

    fix beug sass

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,4 +9,19 @@
 import '../scss/app.scss';
 
 // start the Stimulus application
-import '../bootstrap';
+import 'bootstrap';
+
+// Favorite Button - Heart
+$('.favme').click(function() {
+    $(this).toggleClass('active');
+});
+
+/* when a user clicks, toggle the 'is-animating' class */
+$(".favme").on('click touchstart', function(){
+    $(this).toggleClass('is_animating');
+});
+
+/*when the animation is over, remove the class*/
+$(".favme").on('animationend', function(){
+    $(this).toggleClass('is_animating');
+});
