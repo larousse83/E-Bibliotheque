4180b974290eeeffaeb8e7f663184183ebb9b512
larousse83 <laure.cambours@gmail.com>
2021-01-17 09:27:07 +0100

    refactoring code accountController

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Abonnement;
-use App\Entity\Ouvrage;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\AbonnementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,17 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="app_account")
+     * @Route("/account", name="app_account", methods={"GET"})
      */
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(AbonnementRepository $repo): Response
     {
         if(!$this->getUser()){
             $this->addFlash('error', 'Vous devez vous connecter !.');
 
             return $this->redirectToRoute('app_home');
         }
+        $abonnements =  $repo->findBy(['user' => $this->getUser()]);
 
-        $abonnements =  $entityManager->getRepository(Abonnement::class)->findBy(['user' => $this->getUser()]);
         return $this->render('account/index.html.twig', ['abonnements' => $abonnements]);
     }
 }
